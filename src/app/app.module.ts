7a92d3e1beaee699592311439bed48202f1c1bc7
jiaen188 <244189981@qq.com>
2017-12-17 20:30:02 +0800

    basic of this project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,43 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { StockManageComponent } from './stock/stock-manage/stock-manage.component';
+import { StarsComponent } from './stars/stars.component';
+import {RouterModule, Routes} from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StockFormComponent } from './stock/stock-form/stock-form.component';
+import {StockService} from './stock/stock.service';
 
+const routeConfig: Routes = [
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'stock', component: StockManageComponent},
+  {path: 'stockform/:id', component: StockFormComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    SidebarComponent,
+    FooterComponent,
+    ContentComponent,
+    StockManageComponent,
+    StarsComponent,
+    DashboardComponent,
+    StockFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routeConfig, {useHash: true})
   ],
-  providers: [],
+  providers: [StockService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/stock/stock-manage/stock-manage.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {Stock, StockService} from '../stock.service';
+
+@Component({
+  selector: 'app-stock-manage',
+  templateUrl: './stock-manage.component.html',
+  styleUrls: ['./stock-manage.component.css']
+})
+export class StockManageComponent implements OnInit {
+
+  private stocks: Array<Stock>;
+
+  constructor(public router: Router, private stockService: StockService) { }
+
+  ngOnInit() {
+    this.stocks = this.stockService.getStocks();
+
+  }
+
+  create() {
+    this.router.navigateByUrl('/stockform/0');
+  }
+
+  update(stock: Stock) {
+    this.router.navigateByUrl('/stockform/'+stock.id);
+  }
+
+}
